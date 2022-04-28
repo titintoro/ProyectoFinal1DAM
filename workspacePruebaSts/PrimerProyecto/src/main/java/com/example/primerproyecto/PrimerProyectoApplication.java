72dc122eeda2fa56e6095352185747b5725e2b4c
titintoro <titintola@gmail.com>
2022-04-28 14:24:59 +0200

    Añado workspace y carpetas importantes

---
+++ workspacePruebaSts/PrimerProyecto/src/main/java/com/example/primerproyecto/PrimerProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.primerproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+	}
+
+}
---
+++ workspacePruebaSts/PrimerProyecto/src/test/java/com/example/primerproyecto/PrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primerproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
