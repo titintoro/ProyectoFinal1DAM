b047687b3e43c5202ef83874d27ef6258a5ce47d
titintoro <titintola@gmail.com>
2022-05-11 14:30:30 +0200

    Mismo commit de antes pero se me ha olvidado guardar sts

--- workspacePruebaSts/PruebaProyectoFinal/src/main/java/com/salesianostriana/dam/controladores/PalaController.java
+++ workspacePruebaSts/PruebaProyectoFinal/src/main/java/com/salesianostriana/dam/controladores/PalaController.java
@@ -1,5 +1,13 @@
 package com.salesianostriana.dam.controladores;
 
+import org.springframework.stereotype.Controller;
+
+import com.salesianostriana.dam.pruebaproyectofinal.servicios.PalaServicio;
+
+@Controller
 public class PalaController {
 
+	private PalaServicio palaServicio;
+	
+	
 }
