33b89c86fa18a5f727cdb25248e47d22cd617ce4
titintoro <titintola@gmail.com>
2022-05-18 17:18:53 +0200

    Commit para asegurar el cambio de carrito y solucionarlo

--- workspacePruebaSts/ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/controladores/VentaController.java
+++ workspacePruebaSts/ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/controladores/VentaController.java
@@ -1,21 +1,33 @@
 package com.salesianostriana.dam.proyectofinal.controladores;
 
-import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import com.salesianostriana.dam.proyectofinal.formbeans.SearchBean;
-import com.salesianostriana.dam.proyectofinal.servicios.PalaServicio;
+import com.salesianostriana.dam.proyectofinal.model.Pala;
+import com.salesianostriana.dam.proyectofinal.servicios.ShoppingCartService;
 
 
 @Controller
 public class VentaController {
-	@Autowired
-	HttpSession session;
 	
+	@Autowired
+    private ShoppingCartService shoppingCartService;
+	
+	@ModelAttribute("total_carrito")
+    public Double totalCarrito () {
 
-		
+        Map <Pala,Integer> carrito=shoppingCartService.getPalasEnCarrito();
+        double total=0.0;
+        if (carrito !=null) {
+            for (Pala p: carrito.keySet()) {
+                total+=p.getPrecio()*carrito.get(p);
+            }
+            return total;
+        }
+
+        return 0.0;
+    }
 	}
